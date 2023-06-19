dfdd231bc1bd8fcae8b036b1179238d64190022b
alterNERDtive <nerd@alternerd.tv>
2023-06-19 17:05:08 +0200

    respect fake player fix option

--- src/main/java/tv/alterNERD/VaultModTweaks/integration/mixin/MixinPlayerResearchesData.java
+++ src/main/java/tv/alterNERD/VaultModTweaks/integration/mixin/MixinPlayerResearchesData.java
@@ -12,6 +12,7 @@ import iskallia.vault.init.ModConfigs;
 import iskallia.vault.research.ResearchTree;
 import iskallia.vault.research.type.Research;
 import iskallia.vault.world.data.PlayerResearchesData;
+import tv.alterNERD.VaultModTweaks.Configuration;
 
 @Mixin(PlayerResearchesData.class)
 public class MixinPlayerResearchesData {
@@ -22,7 +23,7 @@ public class MixinPlayerResearchesData {
     public ResearchTree getResearches(UUID uuid) {
         return this.playerMap.computeIfAbsent(uuid, id -> {
             ResearchTree tree = ResearchTree.empty();
-            if (id.equals(UUID.fromString("41c82c87-7afb-4024-ba57-13d2c99cae77"))) {
+            if (Configuration.FAKE_PLAYER_FIX.get() && id.equals(UUID.fromString("41c82c87-7afb-4024-ba57-13d2c99cae77"))) {
                 for (Research research : ModConfigs.RESEARCHES.getAll()) {
                     tree.research(research);
                 }
